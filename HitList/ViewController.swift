a2af5687fe10b8882b48f4e69c6c028943ba7228
Nikita Galkov <tyler_1991@mail.ru>
2021-03-14 16:04:48 +0300

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Galkov Nikita on 14.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
